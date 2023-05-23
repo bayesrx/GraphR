9675ff45ca3c02549dede604857ee2e51bb9e53a
clychenly <70034802+clychenly@users.noreply.github.com>
2023-05-22 23:50:19 -0400

    internal function

--- R/compile.R
+++ R/compile.R
@@ -7,7 +7,6 @@
 #' @param n sample size
 #' @param e_zbeta_sq expectation
 #' @param len_res_sq length
-#' @noRd
 update_lambda_r <- function(n, e_zbeta_sq,len_res_sq){
   e_inv_lambda_new = Egig((n+2)/2,e_zbeta_sq,len_res_sq,
                           func = "1/x")
